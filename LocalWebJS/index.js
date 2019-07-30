84ab75f3bad1c6720bfd2a9aa51a730d9a15a1b5
theTurbanCoder <tannad1@hawk.iit.edu>
2019-07-30 16:47:20 -0500

    p2p 1280 x 720 p tested, works + removed white spaces.

--- LocalWebJS/index.js
+++ LocalWebJS/index.js
@@ -1,24 +1,9 @@
 'use strict';
-
-
-
-
 import ReconnectingWebSocket from 'reconnecting-websocket';
-import { parse } from 'path';
-
 const webSocket = new ReconnectingWebSocket('ws://localhost:8080');
-
-
 console.log(webSocket);
-
-
-
-
-
-
-
+let filearray = ['test3.jpg','test4.jpg','test5.jpg'];
 const MAX_CHUNK_SIZE = 262144;
-
 let bytesToSend = 0;
 let localConnection = 0;
 let remoteConnection;
@@ -26,74 +11,49 @@ let sendChannel;
 let remoteChannel;
 let remoteAnswer = null;
 let receiveChannel;
-
 let localOfferSend = null;
 let remoteCandidate = null;
-
 let localOffer = null;
-
 webSocket.onopen = () => {
-
     webSocket.send(JSON.stringify({username:'localPeer', type:"addUser"}));
-
 }
 
 
 
 webSocket.onmessage = (data) => {
-
    let msg = (JSON.parse(data.data));
-
    console.log(msg);
-
     switch (msg.type) {
-
         case 'sendLocalOffer':
-
         console.log('local Offer')
-
             webSocket.send(JSON.stringify({localOffer:localOffer, type:"offer"}));
-            
-       
-            break;
-        
+        break;
+
         case 'answer':
-            
             console.log('remote Answer',msg.remoteAnswer);
             handleRemoteAnswer(msg.remoteAnswer);
-            break;
+        break;
 
         case 'sendLocalCandidate':
-
-        console.log('send local candidate');
-
-        if (localOfferSend) {
-
-            console.log('local candidate send ',localOfferSend);
-            webSocket.send(JSON.stringify({localCandidate:localOfferSend, type:"localCandidate"}));
-        }
-            
+            console.log('send local candidate');
+            if (localOfferSend) {
+                console.log('local candidate send ',localOfferSend);
+                webSocket.send(JSON.stringify({localCandidate:localOfferSend, type:"localCandidate"}));
+            }
         break;
 
 
-        case 'remoteCandidate':
-            
+        case 'remoteCandidate':   
             handleRemoteCandidate(msg.remoteCandidate);
-        
         break;
-
 
         default:
-            break;
+        break;
     }
-
 }
-
-
 
 async function handleRemoteCandidate(remoteCandidate)
 {
-
     try {
         console.log(remoteCandidate)
         await localConnection.addIceCandidate(remoteCandidate);
@@ -101,7 +61,6 @@ async function handleRemoteCandidate(remoteCandidate)
     } catch (e) {
       console.error('Failed to add Ice Candidate: ', e);
     }
-
 }
 
 
@@ -109,86 +68,44 @@ async function handleRemoteCandidate(remoteCandidate)
 async function createConnection(){
 
     const servers = null;
-
-    const dataChannelParams = {ordered:false};
-
-
+    const dataChannelParams = {ordered:false,binaryType:'blob'};
     localConnection = new RTCPeerConnection(servers);
-
     console.log(localConnection);
     sendChannel = localConnection.createDataChannel('sendDataChannel',dataChannelParams);
     console.log(sendChannel);
-
     sendChannel.onopen = onSendChannelOpen;
     sendChannel.onclose = onSendChannelClose;
-
     sendChannel.addEventListener('message',sendChannelGotData);
-
     localConnection.addEventListener('icecandidate',e => {onIceCandidate(localConnection,e)});
-
     try {
-        
         localOffer = await localConnection.createOffer();
         console.log('local offer has been created', localOffer);
-
         localOfferSend =  localOffer;
-
         await handleDescription(localOffer);
-
-
-
-       
-        
-    
-
-      
-
-
-    } catch (error) {
-        
-    }
-
-
+    } catch (error) {}
 }
-
-
 
 function sendChannelGotData(event)
 {
-
     console.log("Send Channle got", event.data);
-
 }
-
 
 function handleRemoteAnswer(desc)
 {
     console.log("TRmote answer",desc.sdp);
     localConnection.setRemoteDescription(desc);
-
 }
-
 
  function handleDescription(desc)
 {
-
-    
-
     console.log("SDP", desc.sdp);
     localConnection.setLocalDescription(desc)
-
-
 }
 
 
 async function onIceCandidate(pc,e){
-
     const candidate =  e.candidate;
-
     console.log(e.candidate)
-  
-    
-
     if (candidate === null)
     {
         return;
@@ -197,28 +114,36 @@ async function onIceCandidate(pc,e){
         localOfferSend = candidate
         console.log('localOffer',localOfferSend)
     }
-
-
-
-
 }
 
 
 function getOtherPC(pc) {
-
    return pc;
-
-
 }
 
-
-function onSendChannelOpen(){
-    console.log('send Chanel is open');
-    sendChannel.send('Dmitry strygin is still working.')
+async function fetchData(url,filename)
+{
+    var blob =  await fetch(url+filename).then((response) => {
+        return response.blob().then((blob) => blob )
+    });
+    console.log('Blboab', blob);
+    return blob;
 }
 
-// 
+async function onSendChannelOpen(){
+
+    let i = 0;
+    while (i<300) {
+        let randomArr = filearray.sort(() => 0.5 - Math.random() );
+        for (let img in randomArr)
+        {
+            let blob = await fetchData('http://127.0.0.1:5501/stub-data/',`${randomArr[img]}`);
+            console.log('send Chanel is open');
+            sendChannel.send(blob);
+        }        
+        i+=1;
+    }
+}
 
 function onSendChannelClose(){}
-
 createConnection();
--- RemoteWebJS/index.js
+++ RemoteWebJS/index.js
@@ -1,10 +1,6 @@
 'use strict';
-
 import ReconnectingWebSocket from 'reconnecting-websocket';
-
 const webSocket = new ReconnectingWebSocket('ws://localhost:8080');
-
-
 let remoteConnection;
 let localOffer;
 let remoteAnswer;
@@ -12,128 +8,66 @@ let localConnection;
 let localCandidate = null;
 let receiveChannel;
 
-
-
 async function createConnection(){
-
     const servers= null;
-
     remoteConnection = new RTCPeerConnection(servers);
-    
     remoteConnection.addEventListener('icecandidate', e => { onIceCandidate(remoteConnection,e)} )
-
     remoteConnection.addEventListener('datachannel',receiveChannelCallback)
-
-
     console.log(remoteConnection);
-
 }
-
-
 
 async function handleDescription(desc) {
-
     console.log(desc.sdp);
-
     remoteConnection.setRemoteDescription(desc)
-
     try {
-
         remoteAnswer = await remoteConnection.createAnswer();
-
         console.log('remote Answer',remoteAnswer);
-
         handleRemoteAnswer(remoteAnswer);
-
     }
-
     catch(error)
-    {
-
-    }
-
+    {}
 }
-
-
-
 async function handleRemoteAnswer(desc) {
-
-    // console.log('Remote Answer SDP', desc.sdp);
-
     remoteConnection.setLocalDescription(desc);
     console.log('set remote Answer on remote local descritption', desc.sdp);
-
     webSocket.send(JSON.stringify({type:'remoteAnswer',remoteAnswer:remoteAnswer }));
-
- 
-
-
-    // 
-    
-
-
-
-
 }
 
 
 function receiveChannelCallback(event){
-
     console.log('receive channel callback');
     receiveChannel = event.channel
     console.log('Receive Chanenl',receiveChannel)
-    receiveChannel.binaryType ='arraybuffer';
+    receiveChannel.binaryType ='blob';
     receiveChannel.addEventListener('close',onReceiveChannelClosed)
     receiveChannel.addEventListener('message',onReceiveMessageCallback);
-    
-
 }
 
-
-
-function onReceiveMessageCallback(event){
-    console.log("Message:", event.data);
+async function onReceiveMessageCallback(event){
+    console.log("Message:", event);
+    var blob = event.data
+    var urlCreator = window.URL || window.webkitURL;
+    var imageUrl = urlCreator.createObjectURL( blob );
+    console.log('url of image  is ',imageUrl);
+    document.getElementById('bitmapdata').src = imageUrl;
     receiveChannel.send('Tanveer Anand did the work fuck this is basic shit.')
 }
 function onReceiveChannelClosed(){}
-
-
-
-
 function getOtherPc(pc){
-
     return remoteConnection;
 }
-
-
-
-
 async function onIceCandidate(pc,e){
-
     const remoteCandidate = e.candidate;
-
     if (remoteCandidate === null ){
         return
     }
-
     else{
-
         webSocket.send(JSON.stringify({type:'remoteCandidate',remoteCandidate:remoteCandidate}))
-
     }
-
-
-
-    
-
 }
-
-
-
 async function addLocalCandidate(localCandidate)
 {
-
-    try {
+  try {
         console.log(localCandidate)
         await remoteConnection.addIceCandidate(localCandidate);
         console.log('AddIceCandidate successful: ', localCandidate);
@@ -141,56 +75,26 @@ async function addLocalCandidate(localCandidate)
       console.error('Failed to add Ice Candidate: ', e);
     }
 }
-
-
-
-
-
 console.log(webSocket);
-
-
-
 webSocket.onopen = () => {
-
     webSocket.send(JSON.stringify({username:'remotePeer',type:'addUser'}));
-
 }
-
 webSocket.onmessage = (msg) => {
-
-
      msg = JSON.parse(msg.data)
-
     switch (msg.type) {
         case 'offer':
-
         console.log('remote peer',msg.localOffer)
-
         handleDescription(msg.localOffer);
-        
-            
-            break;
-    
+        break;
+
         case 'localCandidate':
-
         console.log('localCandidate',msg.localCandidate);
-
         if (!localCandidate) {
             localCandidate = msg.localCandidate;
             addLocalCandidate(localCandidate)
-             
         }
-
         default:
             break;
     }
-    
-
-   
-
-   
-    
 }
-
-
 createConnection();
